41677365854ca74f98f0723109364974308d85aa
Ashant4u <pyelpale@gmail.com>
2024-02-03 01:19:38 +0000

    Data Preparation ML

---
+++ Data Preparation/401_Feature_Selection_Correlation_Matrix.py
@@ -0,0 +1,12 @@
+####################################################
+
+#Feature Selection using a Simple Correlation Matrix
+
+####################################################
+
+
+import pandas as pd
+
+my_df = pd.read_csv("feature_selection_sample_data.csv")
+
+correlation_matrix = my_df.corr()
